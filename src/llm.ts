7c2c025ee2b64f57b368c58b7bfbc64aee606ca6
judemartin <judemartin.mj@gmail.com>
2025-04-28 17:22:45 -0500

    Summarizing Messages (10 or more messages)

--- src/llm.ts
+++ src/llm.ts
@@ -3,18 +3,22 @@ import { openai } from './ai'
 import { zodFunction, zodResponseFormat } from 'openai/helpers/zod'
 import { systemPrompt as defaultSystemPrompt } from './systemPrompt'
 import { z } from 'zod'
-export const runLLM = async ({
-  messages,
-  tools = [],
-  temperature = 0.1,
-  systemPrompt,
-}: {
-  messages: AIMessage[]
-  tools?: any[]
-  temperature?: number
-  systemPrompt?: string
-}) => {
+import { getSummary } from "./memory.ts";
+
+export const runLLM = async (
+  {
+    messages,
+    tools = [],
+    temperature = 0.1,
+    systemPrompt,
+  }: {
+    messages: AIMessage[]
+    tools?: any[]
+    temperature?: number
+    systemPrompt?: string
+  }) => {
   const formattedTools = tools.map(zodFunction)
+  const summary = await getSummary();
 
   const response = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
@@ -22,7 +26,7 @@ export const runLLM = async ({
     messages: [
       {
         role: 'system',
-        content: systemPrompt || defaultSystemPrompt,
+        content: `${systemPrompt || defaultSystemPrompt}. Conversation so far: ${summary}`,
       },
       ...messages,
     ],
@@ -59,3 +63,14 @@ export const runApprovalCheck = async (userMessage: string) => {
 
   return result.choices[0].message.parsed?.approved
 }
+
+
+export const summarizeMessages = async (messages: AIMessage[]) => {
+  const response = await runLLM({
+    messages,
+    systemPrompt: `Your job is to summarize the given messages to be used in another LLM's system prompt. Summarize it play by play.`,
+    temperature: 1,
+
+  })
+  return response.content || '';
+};
